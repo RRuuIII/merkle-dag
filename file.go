e030723694c56e8d28c145eb52ebbf0522631408
RRuuIII <119320179+RRuuIII@users.noreply.github.com>
2024-04-16 19:32:35 +0800

    Update file.go

--- file.go
+++ file.go
@@ -1,12 +1,19 @@
 package merkledag
 
 const (
-	FILE = iota
+	AddressLength = 20
+	AddressSize   = 20
+	BucketSize    = 3
+	NumNodes      = 100
+	NumPeers      = 100
+	NumBuckets    = 5
+	FILE          = iota
 	DIR
 )
 
 type Node interface {
 	Size() uint64
+	Name() string
 	Type() int
 }
 
@@ -26,4 +33,30 @@ type DirIterator interface {
 	Next() bool
 
 	Node() Node
+}
+
+// ExampleNode 是 Node 接口的一个示例实现
+type ExampleNode struct {
+	ID   string
+	size uint64
+}
+
+type Peer struct {
+	ID   string
+	Size uint64
+	DHT  DHT
+}
+
+type DHT struct {
+	Buckets []Bucket
+}
+
+// Bucket 表示 Kademlia DHT 中的一个桶
+type Bucket struct {
+	Peers []Peer
+}
+
+// KBucket 表示 Kademlia DHT 中的 K_Bucket 算法
+type KBucket struct {
+	Buckets []Bucket
 }
